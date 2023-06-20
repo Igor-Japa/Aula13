261b922d5982b3c24db0056e63379e8e2c6095da
Igor Mattos <igormmattos06@gmail.com>
2023-06-20 20:54:47 -0300

    adiciona estado

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
-const casas = document.querySelectorAll(".casa");
+import { getTabuleiro } from "./state.js";
+
+const eTabuleiro = document.querySelector(".tabuleiro");
 const disco = document.querySelector(".disco");
 
-console.log(casas);
-console.log(disco);
-
-disco.addEventListener('dragstart', comecaArrastar);
-for (let i = 0; i < casas.length; i++) {
-    const casa = casas[i];
-    casa.addEventListener('dragover', passouPorcima);
-    casa.addEventListener('drop', recebeAlgo);
-}
 
 let arrastado = null;
+
+setup();
 
 function comecaArrastar(evento){
     console.log(evento.target);
@@ -30,4 +25,37 @@ function recebeAlgo(evento){
 function passouPorcima(evento){
     evento.preventDefault();
     //console.log(evento.target);
+}
+
+function setup(){
+    const tabuleiro = getTabuleiro();
+    for (let i = 0; i < tabuleiro.length; i++) {
+        const casa = tabuleiro[i];
+        const eCasa = criaCasa(casa);
+        eTabuleiro.appendChild(eCasa);
+    }
+}
+
+function criaCasa(casa){
+    const eCasa = document.createElement('div');
+    eCasa.classList.add("casa");
+    eCasa.addEventListener('dragover', passouPorcima);
+    eCasa.addEventListener('drop', recebeAlgo);
+    if(casa){
+        const eDisco = criaDisco(casa);
+        eCasa.appendChild(eDisco);
+    }
+    return eCasa;
+}
+
+function criaDisco(casa){
+    const eDisco = document.createElement('div');
+    eDisco.classList.add('disco');
+    eDisco.addEventListener('dragstart', comecaArrastar);
+    if(casa === 'p'){
+        eDisco.classList.add('preto');
+    }else{
+        eDisco.classList.add('branco');
+    }
+    return eDisco;
 }
